2b8811f067c50196ba414135781a6f155552dbb4
Rebekah Milford <rmilford86@gmail.com>
2016-07-14 09:51:06 -0400

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+(function(){
+  angular
+    .module("wdiRadio", [
+      "ui.router",
+      "firebase",
+      "songs"
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunction
+    ])
+
+    function RouterFunction($stateProvider){
+      $stateProvider
+        .state("Welcome", {
+          url: "",
+          templateUrl: "js/greetings.html"
+        })
+        .state("songs", {
+          url: "/songs",
+          templateUrl: "js/songs/index.html",
+          controller: "SongsIndexController",
+          controllerAs: "SongsIndexViewModel"
+        })
+    }
+}());
